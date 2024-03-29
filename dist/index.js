05c7039bf5e1502628fb9abe75dee6632138b1d6
teripe <82158483+teripe@users.noreply.github.com>
2024-03-29 10:52:18 +0100

    Another attempt

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const metadata_loader_1 = require("./src/metadata-loader");
-let loader = new metadata_loader_1.MetadataLoader(process.argv[1]);
-loader.readFile().then((content) => {
-    console.log("File downloaded", content);
+const fs_1 = __importDefault(require("fs"));
+console.log('Current working directory:', process.cwd());
+new metadata_loader_1.MetadataLoader(process.argv[1]).readFile().then(content => {
+    console.log('File content:', content);
+    fs_1.default.writeFileSync('metadata.xml', content, 'utf8');
+}).catch(error => {
+    console.error('An error occurred:', error);
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { MetadataLoader } from "./src/metadata-loader";
+import fs from 'fs';
 
-let loader = new MetadataLoader(process.argv[1]);
-loader.readFile().then((content) => {
-    console.log("File downloaded", content);
-})
+console.log('Current working directory:', process.cwd());
+
+new MetadataLoader(process.argv[1]).readFile().then(content => {
+    console.log('File content:', content);
+    fs.writeFileSync('metadata.xml', content, 'utf8');
+}).catch(error => {
+    console.error('An error occurred:', error);
+});
